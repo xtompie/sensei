d17697e97fc09042f2cb0d046f95fd953720d566
Tomasz Pielczyk <tomasz.pielczyk@gmail.com>
2025-08-07 22:05:26 +0200

    feat: implement Sage interface and SageDiscoverer class for message handling

--- src/Shared/Messenger/Messenger.php
+++ src/Shared/Messenger/Messenger.php
@@ -10,6 +10,7 @@ class Messenger
 {
     public function __construct(
         private SubscriberDiscoverer $subscribers,
+        private SageDiscoverer $sages,
     ) {
     }
 
@@ -24,6 +25,19 @@ class Messenger
                 method: $subscriber::class . '::__invoke',
                 args: [$message],
                 main: fn () => $subscriber->__invoke($message),
+            );
+        }
+
+        foreach ($this->sages->instances($message::class) as [$sage, $methodName]) {
+            /** @var object $sage */
+            /** @var string $methodName */
+            if (!method_exists($sage, $methodName)) {
+                continue;
+            }
+            Aop::aop(
+                method: $sage::class . '::' . $methodName,
+                args: [$message],
+                main: fn () => $sage->$methodName($message),
             );
         }
     }
